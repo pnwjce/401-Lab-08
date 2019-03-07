6ce4b256a83c89e52af57dc8e77e646422cb01f9
pnwjce <elliott.jess@gmail.com>
2019-03-06 21:35:23 -0800

    need to branch

--- api-server/scr/models/categories.js
+++ api-server/scr/models/categories.js
@@ -1,18 +1,26 @@
 'use strict';
 
+const uuid = require('uuid/v4');
+
 class Categories {
 
     constructor() {
+        this.database = [];
     }
 
     get(_id) {
+        let response = id ? this.database.filter((record) => record.id === id) : this.database;
+        return Promise.resolve(response);
     }
 
     post(record) {
+        entry.id = uuid();
+        let record = this.sanitize(record);
+        if(record.id) {this.database = }
     }
 
-    put(_id, record) {
-    }
+    put(_id, record)
+
 
     delete(_id) {
     }
